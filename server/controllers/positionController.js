a1cf16cfc32c5887e1f043f0675d665c77cae06c
Julieta Barrios <julietabarriosd@gmail.com>
2023-09-19 18:41:44 -0300

    edit and display positions ok

--- server/controllers/positionController.js
+++ server/controllers/positionController.js
@@ -36,44 +36,41 @@ const removePosition = async (req,res)=>{
     }
 }
 
-// const updateCustomer = async (req,res)=>{
-//     let {newName, email, newAddress}= req.body 
+const editPosition = async (req,res)=>{
+    let {name, newName}= req.body 
     
-//     try{
-//         const findEmail = await Customer.findOne({email})
-//         if (!findEmail){
-//             res.send({ok:true, message:"This email is not registered in Foodies"})
-//         }
-//         else if (findEmail.name == newName && newAddress == findEmail.address){ 
-//           res.json({ok: true, message: "No change was made"});}
-//         else{
-//             await Customer.findOneAndUpdate({email}, {name: newName,address:newAddress})
-//             res.send({ok:true, message:"The customer was successfully updated"})   
-//         }
-//     }
-//     catch(error){
-//         res.send({ok:false,message:{error}})
-//     }
-// }
+    try{
+        const findPosition = await Position.findOne({name})
+        if (!findPosition){
+            res.send({ok:true, message:"This position is not registered"})
+        }
+        else if (name == newName){ 
+          res.json({ok: true, message: "No change was made"});}
+        else{
+            await Position.findOneAndUpdate({name}, {name: newName})
+            res.send({ok:true, message:"The position was successfully updated"})   
+        }
+    }
+    catch(error){
+        res.send({ok:false,message:{error}})
+    }
+}
 
-
-
-
-//   const displayCustomer = async (req,res)=>{
-//     let {email}= req.body
-//     try{
-//         const customerInfo = await Customer.find({email})
-//         res.send({ok:true, message:customerInfo}) 
-//         }
-//     catch(error){
-//         res.send({ok:false,message:{error}})
-//     }
-// }
-
+const displayAllPositions = async (req,res)=>{
+    try{
+        const positions = await Position.find()
+        res.send({ok:true, message:positions})   
+        }
+    catch(error){
+        res.send({ok:false,message:{error}})
+    }
+}
 
 
 
 module.exports={
     addPosition,
-    removePosition
+    removePosition,
+    editPosition,
+    displayAllPositions
 }
--- server/routes/positionRoute.js
+++ server/routes/positionRoute.js
@@ -4,6 +4,9 @@ router        = express.Router(),
 
 router.post('/add', controller.addPosition)
 router.post('/remove', controller.removePosition)
+router.post('/edit', controller.editPosition)
+router.get('/display', controller.displayAllPositions)
+
 
 
 
