441701f3bf869c41e3e196e005358299afd79a2c
Melanie Liendo <meliliendo@hotmail.com>
2023-09-18 21:30:11 -0300

    added .env to connect mongo

--- server/routes/positionRoute.js
+++ server/routes/positionRoute.js
@@ -3,7 +3,6 @@ router        = express.Router(),
     controller    = require('../controllers/positionController');
 
 router.post('/add', controller.addPosition)
-router.post('/edit', controller.editPosition)
 router.post('/remove', controller.removePosition)
 
 
